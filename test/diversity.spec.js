4f77eeba169f29960da7c275681fcc5c17bfa86d
Robert Penner <robert@robertpenner.com>
2015-05-10 00:33:35 +0545

    Created type-safe mechanism for table-based testing of functions.

--- test/diversity.spec.js
+++ test/diversity.spec.js
@@ -7,94 +7,49 @@ var richness = require('../diversity').richness;
 var normalize = require('../diversity').normalize;
 var tolerance = .000000001;
 describe('diversity', function () {
-    it('is a function', function () {
-        expect(diversity).to.be.an.instanceOf(Function);
-    });
-    describe('of 1', function () {
-        var weights = [1];
-        it('is 1', function () {
-            expect(diversity(weights)).to.equal(1);
-        });
-    });
-    describe('2-way split', function () {
-        var weights = [.5, .5];
-        it('is 2', function () {
-            expect(diversity(weights)).to.equal(2);
-        });
-    });
-    describe('5-way split', function () {
-        var weights = [.2, .2, .2, .2, .2];
-        it('is 5', function () {
-            expect(diversity(weights)).to.be.closeTo(5, tolerance);
-        });
+    var testCases = [
+        [[1], 1, 'just 1'],
+        [[.5, .5], 2, 'halved'],
+        [[.2, .2, .2, .2, .2], 5, '5-way split']
+    ];
+    runFunction1Tests(richness, testCases, function (expected, actual) {
+        expect(actual).to.be.closeTo(expected, tolerance);
     });
 });
 describe('richness', function () {
-    var tolerance = .000000001;
     var testCases = [
-        ['just 1', [1], 1],
-        ['halved', [.5, .5], 2],
-        ['3 numbers', [10, 5, 5], 3],
-        ['5-way split', [.2, .2, .2, .2, .2], 5]
+        [[1], 1, 'just 1'],
+        [[.5, .5], 2, 'halved'],
+        [[10, 5, 5], 3, '3 numbers'],
+        [[.2, .2, .2, .2, .2], 5]
     ];
-    testCases.forEach(function (testCase) {
-        var subject = richness;
-        // sweet destructuring action
-        var description = testCase[0], input = testCase[1], expected = testCase[2];
-        var actual = subject(input);
-        var stringify = JSON.stringify;
-        describe(description, function () {
-            it(stringify(input) + " => " + stringify(expected), function () {
-                expect(actual).to.be.closeTo(expected, tolerance);
-            });
-        });
+    runFunction1Tests(richness, testCases, function (expected, actual) {
+        expect(actual).to.be.closeTo(expected, tolerance);
     });
-});
-// might be quixotic
-function runFunction1TestCases(subject, testCases, assertion) {
-    testCases.forEach(function (testCase) {
-        // sweet destructuring action
-        var description = testCase[0], input = testCase[1], expected = testCase[2];
-        var actual = subject(input);
-        var stringify = JSON.stringify;
-        describe(description, function () {
-            var expectation = stringify(input) + " => " + stringify(expected);
-            it(expectation, assertion(expected, actual));
-        });
-    });
-}
-var normalizeTestCases = [
-    ['just 1', [1], [1],],
-    ['halved', [.5, .5], [.5, .5],],
-    ['50/50', [50, 50], [.5, .5],],
-    ['Pareto', [80, 20], [.8, .2],],
-    ['3 numbers', [10, 5, 5], [.5, .25, .25]]
-];
-runFunction1TestCases(normalize, normalizeTestCases, function (expected, actual) {
-    return function () { return expect(actual).to.have.members(expected); };
 });
 describe('normalize', function () {
     var testCases = [
-        ['just 1', [1], [1],],
-        ['halved', [.5, .5], [.5, .5],],
-        ['50/50', [50, 50], [.5, .5],],
-        ['Pareto', [80, 20], [.8, .2],],
-        ['3 numbers', [10, 5, 5], [.5, .25, .25]]
+        [[1], [1], 'just 1'],
+        [[.5, .5], [.5, .5], 'halved'],
+        [[50, 50], [.5, .5], '50/50'],
+        [[80, 20], [.8, .2], 'Pareto'],
+        [[10, 5, 5], [.5, .25, .25], '3 numbers']
     ];
-    testCases.forEach(function (testCase) {
-        var subject = normalize;
-        // sweet destructuring action
-        var description = testCase[0], input = testCase[1], expected = testCase[2];
-        var actual = subject(input);
-        var stringify = JSON.stringify;
-        describe(description, function () {
-            it(stringify(input) + " => " + stringify(expected), function () {
-                expect(actual).to.have.members(expected);
-            });
-        });
+    runFunction1Tests(normalize, testCases, function (expected, actual) {
+        expect(actual).to.have.members(expected);
     });
 });
-//interface Function2<A, B, R> { (a: A, b: B): R }
-//interface Function3<A, B, C, R> { (a: A, b: B, c: C): R }
-//interface Function4<A, B, C, D, R> { (a: A, b: B, c: C, d: D): R }
-//interface Function5<A, B, C, D, E, R> { (a: A, b: B, c: C, d: D, e: E): R }
+////////////////////////////////
+function runFunction1Test(subject, test, assertion) {
+    // sweet destructuring action
+    var input = test[0], expected = test[1], description = test[2];
+    var actual = subject(input);
+    var stringify = JSON.stringify;
+    var expectation = stringify(input) + " => " + stringify(expected);
+    describe(description || expectation, function () {
+        it(expectation, function () { return assertion(expected, actual); });
+    });
+}
+function runFunction1Tests(subject, tests, assertion) {
+    tests.forEach(function (test) { return runFunction1Test(subject, test, assertion); });
+}
--- test/diversity.spec.ts
+++ test/diversity.spec.ts
@@ -1,148 +1,90 @@
 /// <reference path="../typings/node/node.d.ts"/>
 /// <reference path="../typings/mocha/mocha.d.ts"/>
 /// <reference path="../typings/chai/chai.d.ts"/>
+interface Reducer<T> { (ts: T[]): T }
+interface Mapper<T> { (ts: T[]): T[] }
+
 const expect = require('chai').expect;
-const diversity = require('../diversity').diversity;
-const richness = require('../diversity').richness;
-const normalize = require('../diversity').normalize;
+const diversity: Reducer<number> = require('../diversity').diversity;
+const richness: Reducer<number> = require('../diversity').richness;
+const normalize: Mapper<number> = require('../diversity').normalize;
 
 const tolerance = .000000001;
 
 describe('diversity', function() {
-  it('is a function', function() {
-    expect(diversity).to.be.an.instanceOf(Function);
-  });
-
-  describe('of 1', function () {
-    var weights = [1];
-    it('is 1', function () {
-      expect(diversity(weights)).to.equal(1);
-    });
-
-  });
-
-  describe('2-way split', function () {
-    var weights = [.5, .5];
-    it('is 2', function () {
-      expect(diversity(weights)).to.equal(2);
-    });
-  });
-
-  describe('5-way split', function () {
-    var weights = [.2, .2, .2, .2, .2];
-    it('is 5', function () {
-      expect(diversity(weights)).to.be.closeTo(5, tolerance);
-    });
-  });
-});
-
-//TODO: base generics on input/output types of the function under test
-type NumbersReduceTestCase = TestCase<number[], number>;
-type NumbersMapTestCase = TestCase<number[], number[]>;
-
-describe('richness', function() {
-  const tolerance = .000000001;
-
-  const testCases: NumbersReduceTestCase[] = [
-      ['just 1',      [1],                    1      ]
-    , ['halved',      [.5, .5],               2      ]
-    , ['3 numbers',   [10, 5, 5],             3      ]
-    , ['5-way split', [.2, .2, .2, .2, .2],   5      ]
+  const testCases: Fn1Test<number[], number>[] = [
+      [[1],                  1, 'just 1']
+    , [[.5, .5],             2, 'halved']
+    , [[.2, .2, .2, .2, .2], 5, '5-way split']
   ];
 
-  testCases.forEach(testCase => {
-    const subject = richness;
-
-    // sweet destructuring action
-    const [ description, input, expected ] = testCase;
-    const actual = subject(input);
-    const stringify = JSON.stringify;
-    describe(description, function() {
-      it(`${ stringify(input) } => ${ stringify(expected) }`, function() {
-        expect(actual).to.be.closeTo(expected, tolerance);
-      })
-    })
+  runFunction1Tests(richness, testCases, function(expected, actual) {
+    expect(actual).to.be.closeTo(expected, tolerance);
   });
-
 });
 
+describe('richness', function() {
+  const testCases: Fn1Test<number[], number>[] = [
+      [[1],                  1, 'just 1']
+    , [[.5, .5],             2, 'halved']
+    , [[10, 5, 5],           3, '3 numbers']
+    , [[.2, .2, .2, .2, .2], 5]
+  ];
 
-
-
-// might be quixotic
-function runFunction1TestCases<A, Result>(subject: Function1<A, Result>,
-                                          testCases: TestCase<A, Result>[],
-                                          assertion: (expected: Result, actual: Result) => () => void) {
-  testCases.forEach(testCase => {
-    // sweet destructuring action
-    const [ description, input, expected ] = testCase;
-    const actual = subject(input);
-    const stringify = JSON.stringify;
-    describe(description, function () {
-      const expectation = `${ stringify(input) } => ${ stringify(expected) }`;
-      it(expectation, assertion(expected, actual));
-    });
+  runFunction1Tests(richness, testCases, function(expected, actual) {
+    expect(actual).to.be.closeTo(expected, tolerance);
   });
-}
-
-const normalizeTestCases: NumbersMapTestCase[] = [
-  ['just 1',    [1],          [1],           ]
-  , ['halved',    [.5, .5],     [.5, .5],      ]
-  , ['50/50',     [50, 50],     [.5, .5],      ]
-  , ['Pareto',    [80, 20],     [.8, .2],      ]
-  , ['3 numbers', [10, 5, 5],   [.5, .25, .25] ]
-];
-
-runFunction1TestCases(normalize, normalizeTestCases, function(expected, actual) {
-  return () => expect(actual).to.have.members(expected);
 });
 
 describe('normalize', function() {
-  const testCases: NumbersMapTestCase[] = [
-      ['just 1',    [1],          [1],           ]
-    , ['halved',    [.5, .5],     [.5, .5],      ]
-    , ['50/50',     [50, 50],     [.5, .5],      ]
-    , ['Pareto',    [80, 20],     [.8, .2],      ]
-    , ['3 numbers', [10, 5, 5],   [.5, .25, .25] ]
+
+  const testCases: Fn1Test<number[], number[]>[] = [
+      [[1],        [1],            'just 1']
+    , [[.5, .5],   [.5, .5],       'halved']
+    , [[50, 50],   [.5, .5],       '50/50']
+    , [[80, 20],   [.8, .2],       'Pareto']
+    , [[10, 5, 5], [.5, .25, .25], '3 numbers']
   ];
 
-  testCases.forEach(testCase => {
-    const subject = normalize;
-
-    // sweet destructuring action
-    const [ description, input, expected ] = testCase;
-    const actual = subject(input);
-    const stringify = JSON.stringify;
-    describe(description, function() {
-      it(`${ stringify(input) } => ${ stringify(expected) }`, function() {
-        expect(actual).to.have.members(expected);
-      });
-    });
+  runFunction1Tests(normalize, testCases, function(expected, actual) {
+    expect(actual).to.have.members(expected);
   });
+
 });
 
+////////////////////////////////
 
-interface TestCase<Input, Expected> extends Array<string | Input | Expected> {
-  0: string; // description
-  1: Input;
-  2: Expected;
+function runFunction1Test<A, Result>(subject: Fn1<A, Result>,
+                                     test: Fn1Test<A, Result>,
+                                     assertion: (expected: Result, actual: Result) => void) {
+  // sweet destructuring action
+  const [ input, expected, description ] = test;
+  const actual = subject(input);
+  const stringify = JSON.stringify;
+  const expectation = `${ stringify(input) } => ${ stringify(expected) }`;
+  describe(description || expectation, function () {
+    it(expectation, () => assertion(expected, actual));
+  });
 }
 
-//interface FnTestCase<Fn extends Function> extends Array<string | Input | Expected> {
-//  0: string; // description
-//  1: Input;
-//  2: Expected;
-//}
-
-interface Fn1TestCase<A, Result> extends Array<string | A | Result> {
-  0: string; // description
-  1: A;
-  2: Result;
+function runFunction1Tests<A, Result>(subject: Fn1<A, Result>,
+                                      tests: Fn1Test<A, Result>[],
+                                      assertion: (expected: Result, actual: Result) => void) {
+  tests.forEach(test => runFunction1Test(subject, test, assertion));
 }
 
-//interface Function0<R> { (): R }
-interface Function1<A, Result> { (a: A): Result }
-//interface Function2<A, B, R> { (a: A, b: B): R }
-//interface Function3<A, B, C, R> { (a: A, b: B, c: C): R }
-//interface Function4<A, B, C, D, R> { (a: A, b: B, c: C, d: D): R }
-//interface Function5<A, B, C, D, E, R> { (a: A, b: B, c: C, d: D, e: E): R }
+interface Fn1<A, Result> { (a: A): Result }
+interface Fn2<A, B, Result> { (a: A, b: B): Result }
+
+interface Fn1Test<A, Result> extends Array<A | Result | string> {
+  0: A;
+  1: Result;  // expected
+  2?: string; // description
+}
+
+interface Fn2Test<A, B, Result> extends Array<A | B | Result | string> {
+  0: A;
+  1: B;
+  2: Result;  // expected
+  3?: string; // description
+}
